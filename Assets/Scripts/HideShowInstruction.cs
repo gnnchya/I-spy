abc3a82ff4134d3aafb22a7bc9a088063a331975
gnnchya <75212443+gnnchya@users.noreply.github.com>
2023-03-17 01:18:41 +0700

    [feature] Add Home page

---
+++ Assets/Scripts/HideShowInstruction.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideShowInstruction : MonoBehaviour
+{
+    public GameObject instruction;
+    
+    public void Show()
+    {
+        instruction.SetActive(true);
+    }
+
+    public void Hide()
+    {
+        instruction.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/ImageScaler.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ImageScaler : MonoBehaviour
+{
+    public Image image;
+    public float scaleFactor = 0.5f; // adjust this to change the scale factor
+
+    void Start()
+    {
+        float screenWidth = Screen.width;
+        float screenHeight = Screen.height;
+
+        float imageWidth = image.sprite.rect.width;
+        float imageHeight = image.sprite.rect.height;
+
+        float screenRatio = screenWidth / screenHeight;
+        float imageRatio = imageWidth / imageHeight;
+
+        float scale = 0.2f;
+
+        if (screenRatio > imageRatio)
+        {
+            Debug.Log("1");
+            scale = screenHeight / imageHeight * scaleFactor;
+        }
+        else
+        {
+            Debug.Log("2");
+            scale = screenWidth / imageWidth * scaleFactor;
+        }
+
+        image.GetComponent<RectTransform>().localScale = new Vector3(scale, scale, 1f);
+    }
+}
