ecf16574f9797ab4b96127a3c8d3d6a5b6e576d1
gnnchya <75212443+gnnchya@users.noreply.github.com>
2023-03-17 14:56:26 +0700

    [feature] add instruction page

---
+++ Assets/Scripts/buttonSound.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buttonSound : MonoBehaviour
+{
+    public AudioSource audio;
+
+    public void click()
+    {
+        audio.Play();
+    }
+}
