8156dce1c34b85b701d072e43a532c0ff34c1ec0
flukekd <flukekd@gmail.com>
2023-05-23 00:50:18 +0700

    update screen

--- Assets/Scripts/RawImageScaler.cs
+++ Assets/Scripts/RawImageScaler.cs
@@ -13,9 +13,15 @@ public class RawImageScaler : MonoBehaviour
     void ScaleRawImage()
     {
         RectTransform rectTransform = rawImage.rectTransform;
-        rectTransform.anchorMin = Vector2.zero;
-        rectTransform.anchorMax = Vector2.one;
-        rectTransform.sizeDelta = Vector2.zero;
-        rectTransform.anchoredPosition = Vector2.zero;
+        // rectTransform.anchorMin = Vector2.zero;
+        // rectTransform.anchorMax = Vector2.one;
+        // rectTransform.sizeDelta = Vector2.zero;
+        // // rectTransform.anchoredPosition = Vector2.zero;
+        // float screenWidth = Screen.width;
+        // rectTransform.Height = Screen.width;
+        rectTransform.sizeDelta = new Vector2(Screen.height, Screen.width);
+        rectTransform.Rotate(new Vector3(0, 0, 90));
+        rectTransform.localScale = new Vector3(-1, 1, 1);
     }
+
 }
